d66b466123ea22857317832b93949c39326cb9f6
Frederik Shull <zlyfer@protonmail.com>
2018-04-14 19:46:08 +0200

    Initial Commit

---
+++ sketch.js
@@ -0,0 +1,172 @@
+var score, speed, stime, length, borderkill;
+var snake, body, food;
+var oldsecond, newdir;
+var speedslider, borderkillcheckbox;
+var gameover, time;
+var dir;
+
+function preload() {}
+
+function setup() {
+  createCanvas(854, 604);
+
+  initInputs();
+  newGame();
+}
+
+function draw() {
+  translate(2, 2);
+  frameRate(60);
+  background(100);
+
+  showField();
+  showScore();
+
+  gameover = snake.gameover(body);
+  if (gameover) {
+    endGame();
+  } else {
+    playGame();
+  }
+}
+
+function initInputs() {
+  speedslider = createSlider(1, 25, 8);
+  speedslider.position(745, 550);
+  speedslider.style('width', '100px');
+  borderkillcheckbox = createCheckbox('', true);
+  borderkillcheckbox.style('background-color', 'rgb(244, 67, 54)');
+  borderkillcheckbox.position(830, 580);
+}
+
+function newGame() {
+  var ix = random([2, 7, 12]) * 40;
+  var iy = random([2, 7, 12]) * 40;
+  body = [];
+  snake = new Snake(ix, iy, 40);
+  food = new Food(snake, body);
+  time = score = stime = length = 0;
+  gameover = dir = newdir = false;
+  oldsecond = second();
+  speed = speedslider.value();
+  borderkill = borderkillcheckbox.checked();
+}
+
+function showScore() {
+  fill(33);
+  textAlign(LEFT);
+
+  textSize(20);
+  text('Settings', 610, 535);
+
+  textSize(18);
+  text('Speed', 610, 560);
+  text('Borderkill', 610, 585);
+
+  if (gameover || stime == 0) {
+    speed = speedslider.value();
+  }
+
+  textSize(20);
+
+  textAlign(LEFT);
+  text('Speed:', 610, 25);
+  text('Time:', 610, 50);
+  text('Score:', 610, 75);
+  text('Food:', 610, 100);
+
+  textAlign(RIGHT);
+  text(speed, 840, 25);
+  text(stime, 840, 50);
+  text(score, 840, 75);
+  text(length, 840, 100);
+}
+
+function showField() {
+  strokeWeight(0);
+
+  fill(244, 67, 54);
+  rect(602, -2, 250, 604);
+
+  fill(200);
+  for (let i = 0; i < 600; i += 40) {
+    for (let j = 0; j < 600; j += 40) {
+      rect(i + 1, j + 1, 38, 38);
+    }
+  }
+}
+
+function showGame() {
+  food.show();
+  snake.show();
+  body.forEach(part => {
+    part.show();
+  });
+}
+
+function endGame() {
+  stroke(255);
+  strokeWeight(3);
+  fill(229, 57, 53);
+  textAlign(CENTER);
+
+  textSize(64);
+  text('GAME OVER!', 300, 300);
+  textSize(32);
+  text('PRESS \'R\' TO RESTART', 300, 350);
+}
+
+function playGame() {
+  if (time < speed) {
+    time++;
+  }
+
+  if (time == speed) {
+    dir = newdir;
+    if (body.length > 0) {
+      for (let i = body.length - 1; i >= 0; i--) {
+        body[i].follow();
+      }
+    }
+    snake.move(dir);
+    time = 0;
+  }
+
+  if (!(food)) {
+    food = new Food(snake, body);
+    length++;
+    if (body.length == 0) {
+      body.push(new Body(snake));
+    } else {
+      body.push(new Body(body[body.length - 1]));
+    }
+  }
+
+  snake.teleport();
+  showGame();
+
+  food = snake.eat(food);
+  if (borderkill) {
+    score = round((27 - speed) * length);
+  } else {
+    score = round((27 - speed) * (length / 2));
+  }
+
+  if (!(dir)) {
+    oldsecond = second();
+  }
+  if (oldsecond != second()) {
+    stime++;
+    oldsecond = second();
+  }
+}
+
+function keyPressed() {
+  if ([37, 38, 39, 40].indexOf(keyCode) != -1) {
+    if (dir - keyCode != 2 && dir - keyCode != -2) {
+      newdir = keyCode;
+    }
+  } else if (["R", "r"].indexOf(key) != -1) {
+    newGame();
+  }
+}
---
+++ snake.js
@@ -0,0 +1,114 @@
+class Snake {
+  constructor(x, y, size) {
+    this.x = x;
+    this.y = y;
+  }
+  eat(food) {
+    if (food.x == this.x && food.y == this.y) {
+      return false;
+    } else {
+      return food;
+    }
+  }
+  teleport() {
+    if (!(borderkill)) {
+      if (this.x + 40 > 600) {
+        this.x = 0;
+      }
+      if (this.x < 0) {
+        this.x = 560;
+      }
+      if (this.y < 0) {
+        this.y = 560;
+      }
+      if (this.y + 40 > 600) {
+        this.y = 0;
+      }
+    }
+  }
+  gameover(body) {
+    if (gameover) {
+      return true;
+    }
+
+    if (borderkill) {
+      if (
+        this.x + 40 > 600 ||
+        this.x < 0 ||
+        this.y < 0 ||
+        this.y + 40 > 600
+      ) {
+        return true;
+      }
+    }
+
+    for (let i = 0; i < body.length; i++) {
+      if (this.x == body[i].x && this.y == body[i].y) {
+        return true;
+      }
+    }
+    return false;
+  }
+  move(dir) {
+    switch (dir) {
+      case 37:
+        this.x -= 40;
+        break;
+      case 38:
+        this.y -= 40;
+        break;
+      case 39:
+        this.x += 40;
+        break;
+      case 40:
+        this.y += 40;
+        break;
+    }
+  }
+  show() {
+    strokeWeight(0);
+    fill(46, 125, 50);
+    rect(this.x + 1, this.y + 1, 38, 38);
+  }
+}
+
+class Body {
+  constructor(attach) {
+    this.x = -40;
+    this.y = -40;
+    this.attach = attach;
+  }
+  follow() {
+    this.x = this.attach.x;
+    this.y = this.attach.y;
+  }
+  show() {
+    strokeWeight(0);
+    fill(76, 175, 80);
+    rect(this.x + 1, this.y + 1, 38, 38);
+  }
+}
+
+class Food {
+  constructor(snake, body) {
+    this.x = round(random(14)) * 40;
+    this.y = round(random(14)) * 40;
+    while (this.x == snake.x && this.y == snake.y) {
+      this.x = round(random(14)) * 40;
+      this.y = round(random(14)) * 40;
+    }
+    if (body.length > 0) {
+      for (let i = 0; i < body.length; i++) {
+        while (this.x == body[i].x && this.y == body[i].y) {
+          this.x = round(random(14)) * 40;
+          this.y = round(random(14)) * 40;
+        }
+      }
+    }
+  }
+  show() {
+    strokeWeight(0);
+    fill(33, 150, 243);
+    rect(this.x + 5, this.y + 5, 30, 30);
+  }
+}
